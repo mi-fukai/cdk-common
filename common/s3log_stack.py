403860def4155e03002af97d93f1d75263e95ab8
mi-fukai <ec2-user@ip-172-31-48-115.us-west-2.compute.internal>
2022-02-22 08:21:13 +0000

    first commit

---
+++ common/s3log_stack.py
@@ -0,0 +1,17 @@
+import aws_cdk as cdk
+from aws_cdk import (
+    Stack,
+    cloudformation_include as cfn_inc,
+)
+from constructs import Construct
+import common.constants
+
+class S3LogStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        template = cfn_inc.CfnInclude(self, "Template",
+            template_file="01-Common-01-S3Log.yml",
+            parameters=dict(ServiceName=common.constants.SERVICE_NAME),
+            preserve_logical_ids=False)
---
+++ common/vpc_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as cdk
+from aws_cdk import (
+    Stack,
+    cloudformation_include as cfn_inc,
+)
+from constructs import Construct
+
+class VpcStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        template = cfn_inc.CfnInclude(self, "Template",
+            template_file="01-Common-02-VpcSubnet.yml",
+            preserve_logical_ids=False)
