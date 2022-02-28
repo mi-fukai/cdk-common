2ea7f7c7f662386e5bc4d1c43760f8eb3dbd1718
mi-fukai <okikimenos@gmail.com>
2022-02-28 16:43:06 +0900

    update

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import os
 
 import aws_cdk as cdk
 
-from common.common_01_s3log_stack import Common_01_S3LogStack
+from common.common_01_s3log_stack import Common-01-S3LogStack
 from common.common_02_vpc_stack import Common_02_VpcStack
 from common.common_03_sg_stack import Common_03_SGStack
 
 app = cdk.App()
-Common_01_S3LogStack(app, "Common_01_S3LogStack",)
+Common-01-S3LogStack(app, "Common-01-S3LogStack",)
 Common_02_VpcStack(app, "Common_02_VpcStack",)
 Common_03_SGStack(app, "Common_03_SGStack",)
 app.synth()
