afdc6309d1857345fa54b8316ddcc88ccf711fa1
mi-fukai <okikimenos@gmail.com>
2022-03-01 11:04:31 +0900

    update

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import os
 
 import aws_cdk as cdk
 
-from common.c01_s3log_stack import S3LogStack
+from common.c01_s3log_stack import c01-S3LogStack
 from common.c02_vpc_stack import VpcStack
 from common.c03_securitygroup_stack import SecurityGroupStack
 from common.c04_sns_stack import SnsStack
 
 app = cdk.App()
-S3LogStack(app, "S3LogStack",)
+c01-S3LogStack(app, "c01-S3LogStack",)
 VpcStack(app, "VpcStack",)
 SecurityGroupStack(app, "SecurityGroupStack",)
 SnsStack(app, "SnsStack",)
--- common/c01_s3log_stack.py
+++ common/c01_s3log_stack.py
@@ -6,7 +6,7 @@ from aws_cdk import (
 from constructs import Construct
 import common.constants
 
-class S3LogStack(Stack):
+class c01-S3LogStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
